8b82e804c0915ed6230ffb4352f3a5c9eee6793b
donghee290 <eehd1127@naver.com>
2026-01-14 16:50:04 +0900

    사진 에셋 추가~

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -6,6 +6,11 @@ class ImageConstants {
     "1총든페페.png",
     "1하품.png",
     "1화난페페.png",
+    "1루피욕설.png",
+    "1셀프관짝.png",
+    "1참자참아.png",
+    "1페페죽창.png",
+    "1화난남자.png",
   ];
 
   static const List<String> imagelevel2List = [
@@ -15,6 +20,11 @@ class ImageConstants {
     "2우는페페.png",
     "2죽빵페페.png",
     "2하기싫어.png",
+    "2루피담배.png",
+    "2말을듣지않는육체.png",
+    "2아침고민.png",
+    "2아침형저녁형.png",
+    "2지치고고된아침.png",
   ];
 
   static const List<String> imagelevel3List = [
@@ -23,6 +33,11 @@ class ImageConstants {
     "3멍.png",
     "3커피페페.png",
     "3흑백페페.png",
+    "3안죽는다일어나.png",
+    "3인제인남.png",
+    "3최민용기상.png",
+    "3탄력기상제.png",
+    "3일어난고양이.png",
   ];
 
   static const List<String> imagelevel4List = [
@@ -30,6 +45,12 @@ class ImageConstants {
     "4뽀뽀페페.png",
     "4웃는페페.png",
     "4흐뭇페페.png",
+    "4박진영.png",
+    "4윤후좋은아침.png",
+    "4짱구5시기상.png",
+    "4일어난내모습.png",
+    "4햄토리.png",
+    "4눈이안떠짐.png",
   ];
 
   static const List<String> imagelevel5List = [
@@ -38,5 +59,10 @@ class ImageConstants {
     "5키자루페페.png",
     "5한잔해페페.png",
     "5흡족.png",
+    "5기분좋은날.png",
+    "5기분좋은박명수.png",
+    "5남자다운기상.png",
+    "5오늘은너의날.png",
+    "5이말년기상.png",
   ];
 }
